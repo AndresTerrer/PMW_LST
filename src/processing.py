d51134a6ea34d544a2c543949b5bc373d1e05d49
Andrés Terrer Gómez <andres.terrer@datamaran.com>
2023-11-23 14:40:58 +0100

    Black formating of .py files

--- src/processing.py
+++ src/processing.py
@@ -7,17 +7,18 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from tqdm import tqdm
 
+
 def holmes(brightness_temp: xr.Dataset) -> xr.Dataset:
     """
-        Calculates the surface temperature from the brightness temperature using Holmes formula
-        only valid for Tb (V) and avobe 259.8 K (Brightness Temperature)
-        :param brightness_temp:
-        :return: 
+    Calculates the surface temperature from the brightness temperature using Holmes formula
+    only valid for Tb (V) and avobe 259.8 K (Brightness Temperature)
+    :param brightness_temp:
+    :return:
     """
     a = 1.11
     b = -15.2
 
-    return  a * brightness_temp + b
+    return a * brightness_temp + b
 
 
 def apply_scaling(xarray_dataset: xr.Dataset) -> xr.Dataset:
@@ -27,7 +28,9 @@ def apply_scaling(xarray_dataset: xr.Dataset) -> xr.Dataset:
     :param xarray_dataset: The input Xarray Dataset.
     :return: A new Xarray Dataset with scaled variables.
     """
-    scaled_dataset = xarray_dataset.copy()  # Create a copy to avoid modifying the original dataset
+    scaled_dataset = (
+        xarray_dataset.copy()
+    )  # Create a copy to avoid modifying the original dataset
     print("Applying scaling")
     for dvar in tqdm(scaled_dataset.data_vars):
         if "SCALE FACTOR" in scaled_dataset[dvar].attrs.keys():
@@ -38,25 +41,28 @@ def apply_scaling(xarray_dataset: xr.Dataset) -> xr.Dataset:
 
     return scaled_dataset
 
-def load_zip(path)-> xr.Dataset:
+
+def load_zip(path) -> xr.Dataset:
     """
-        Creates a Xarray Dataset form a zip file, located in the given path string.
-        also adds a passing label for equatorial ascending and descending passes
-        :params path: str
-        :return: xr.Dataset
-    """ 
+    Creates a Xarray Dataset form a zip file, located in the given path string.
+    also adds a passing label for equatorial ascending and descending passes
+    :params path: str
+    :return: xr.Dataset
+    """
     # Open the ZIP archive and list the file names
-    with zipfile.ZipFile(path, 'r') as zip_file:
+    with zipfile.ZipFile(path, "r") as zip_file:
         file_list = zip_file.namelist()
 
         # Initialize an empty list to store Xarray datasets
         xarray_datasets = []
 
         # Regular expression pattern to match the date in the file name
-        date_pattern = r'_(\d{8})_\d{2}'
+        date_pattern = r"_(\d{8})_\d{2}"
 
         # Initialize a variable for the default date
-        default_date = datetime(2016, 12, 31)  # Set to one day earlier than the first valid date
+        default_date = datetime(
+            2016, 12, 31
+        )  # Set to one day earlier than the first valid date
         print(f"Loading zipfile from {path}")
         # Iterate through the files in the ZIP archive
         for file_name in tqdm(file_list):
@@ -88,14 +94,14 @@ def load_zip(path)-> xr.Dataset:
                     file_class = "Ascending" if "EQMA" in file_name else "Descending"
 
                     # Add the 'time' and 'class' coordinates to the dataset
-                    h5_xarray['time'] = date
-                    h5_xarray['Pass'] = file_class
+                    h5_xarray["time"] = date
+                    h5_xarray["Pass"] = file_class
 
                     # Append the dataset to the list
                     xarray_datasets.append(h5_xarray)
 
         # Concatenate the list of datasets
-        xarray_dataset = xr.concat(xarray_datasets, dim='time')
+        xarray_dataset = xr.concat(xarray_datasets, dim="time")
 
     # Now, xarray_dataset contains all data with 'time' and 'class' coordinates to distinguish between EQMA and EQMD
     xarray_dataset = xarray_dataset.set_coords("Pass")
@@ -103,44 +109,45 @@ def load_zip(path)-> xr.Dataset:
     return xarray_dataset
 
 
-def extract_timeseries(xarray_dataset,lat,lon) -> xr.DataArray:
+def extract_timeseries(xarray_dataset, lat, lon) -> xr.DataArray:
     """
-        Extract the timeseries from a pixel in the map for all instances of time
+    Extract the timeseries from a pixel in the map for all instances of time
 
-        :param xarray_dataset: Dataset with at least lat lon and time coordinates
-        :param lat: float
-        :param lon: float
-        :return: np.array 1D
-    """ 
+    :param xarray_dataset: Dataset with at least lat lon and time coordinates
+    :param lat: float
+    :param lon: float
+    :return: np.array 1D
+    """
 
     timeseries = np.zeros(shape=(len(xarray_dataset.time),))
 
     for i in range(len(xarray_dataset.time)):
-        timeseries[i] = xarray_dataset[i,lat,lon]
+        timeseries[i] = xarray_dataset[i, lat, lon]
 
     return np.array(timeseries)
 
 
-def create_landmask(xarray_dataset, threshold = 273, show = False, file_path = None, figure_path = None) -> xr.DataArray:
+def create_landmask(
+    xarray_dataset, threshold=273, show=False, file_path=None, figure_path=None
+) -> xr.DataArray:
     """
-        Creates a mask for latitude and longitude given a threshold and a base array.
-        Save the resulting xr.DataArray as a file if a path is provided.
+    Creates a mask for latitude and longitude given a threshold and a base array.
+    Save the resulting xr.DataArray as a file if a path is provided.
 
-        :param xarray_dataset: xr.DataSet with latitude and longitude dimensions
-        :param threshold: float
-        :return: xr.DataArray with latitude and longitude
+    :param xarray_dataset: xr.DataSet with latitude and longitude dimensions
+    :param threshold: float
+    :return: xr.DataArray with latitude and longitude
     """
-    landmask = xarray_dataset.where(xarray_dataset > threshold, other= 0)
-    landmask = landmask.where(landmask == 0, other= 1)
-    
+    landmask = xarray_dataset.where(xarray_dataset > threshold, other=0)
+    landmask = landmask.where(landmask == 0, other=1)
+
     # Save the mask into a file option
     if file_path is not None:
         landmask.to_netcdf(file_path)
     if show:
-        landmask.plot(cmap = "jet")
+        landmask.plot(cmap="jet")
         # Save an image of the mask option
         if figure_path is not None:
             plt.savefig(figure_path)
 
     return landmask
-
--- utils/visuals.py
+++ utils/visuals.py
@@ -4,19 +4,19 @@ import numpy as np
 
 def show_map(image: np.array, title=None, save_path=None):
     """
-        This function will show a map of the image
-        :param save_path:
-        :param title:
-        :param image:
+    This function will show a map of the image
+    :param save_path:
+    :param title:
+    :param image:
     """
     # Fix the orientation issue by using coordinates
     image = np.flip(image, axis=0)
 
-    plt.figure(figsize=(15,15))
-    plt.imshow(image, cmap='jet')
+    plt.figure(figsize=(15, 15))
+    plt.imshow(image, cmap="jet")
     cbar = plt.colorbar()
-    cbar.set_label('Average Surface Temperature (K)')
-    plt.axis('off')
+    cbar.set_label("Average Surface Temperature (K)")
+    plt.axis("off")
     if title is not None:
         plt.title(title)
     if save_path is not None:
@@ -38,29 +38,35 @@ def histogram_matrix(h5file, save_path=None):
 
     fig, axs = plt.subplots(row_number, col_number, figsize=(10, 20))
     for i, field in enumerate(fields):
-        if field != 'lat' & field != 'lon':
-            axs[i // col_number, i % col_number].hist(h5file[field][:].flatten(), bins=100)
+        if field != "lat" & field != "lon":
+            axs[i // col_number, i % col_number].hist(
+                h5file[field][:].flatten(), bins=100
+            )
             axs[i // col_number, i % col_number].set_title(field)
 
         if save_path is not None:
-            plt.savefig(f'{save_path}.png')
+            plt.savefig(f"{save_path}.png")
     plt.show()
 
 
-def column_plot(xarray_dataset, save_path = None):
+def column_plot(xarray_dataset, save_path=None):
     """
-        Creates the subplots for each of the passes in the whole dataset
+    Creates the subplots for each of the passes in the whole dataset
     """
-    ncols = 2 # Separate ascending and descending passes into two columns
-    nrows = len(xarray_dataset.time) // 2 + 1 if len(xarray_dataset.time) % 2 == 1 else len(xarray_dataset.time) // 2
-    fig, axs = plt.subplots(ncols=ncols, nrows=nrows, figsize=(10,12))
+    ncols = 2  # Separate ascending and descending passes into two columns
+    nrows = (
+        len(xarray_dataset.time) // 2 + 1
+        if len(xarray_dataset.time) % 2 == 1
+        else len(xarray_dataset.time) // 2
+    )
+    fig, axs = plt.subplots(ncols=ncols, nrows=nrows, figsize=(10, 12))
 
     for i in range(len(xarray_dataset.time)):
         row = i // ncols
         col = i % ncols
 
-        xarray_dataset["Holmes_LST"][i,:,:].plot(ax = axs[row,col], cmap = "jet")
+        xarray_dataset["Holmes_LST"][i, :, :].plot(ax=axs[row, col], cmap="jet")
     plt.tight_layout()
 
     if save_path is not None:
-        plt.savefig(save_path) 
+        plt.savefig(save_path)
