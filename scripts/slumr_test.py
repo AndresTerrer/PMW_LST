c5ced9bd38cc3bea035ed7dccbbaa6e5285c9140
andre <andrescaudiel@gmail.com>
2024-04-09 09:47:03 +0200

    typo

--- scripts/slumr_test.py
+++ scripts/slumr_test.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Generate random matrix
-matriz = np.random.rand(30, 30)
+matrix = np.random.rand(30, 30)
 
 # Save and display the matrix as an image
-plt.imshow(matriz, cmap="viridis", interpolation="nearest")
+plt.imshow(matrix, cmap="viridis", interpolation="nearest")
 plt.colorbar()
 plt.savefig("random_matrix.png")
 plt.show()
