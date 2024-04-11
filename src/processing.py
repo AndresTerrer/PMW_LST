5d593b16af75308d915127ae3ecc9c0dfff3c3b6
andre <andrescaudiel@gmail.com>
2024-04-11 11:21:21 +0200

    windsat loading moved into src.processing

--- src/processing.py
+++ src/processing.py
@@ -2,9 +2,11 @@ import zipfile
 import io
 import xarray as xr
 import re
+import os
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import datetime
+from datetime import datetime, date
+from functools import partial
 from tqdm import tqdm
 
 
@@ -151,3 +153,152 @@ def create_landmask(
             plt.savefig(figure_path)
 
     return landmask
+
+def recover_dates(folder_path: str)-> list[date]:
+    """Helper, returns a list of dates parsing the files within the folder"""
+
+    dates = []	
+    ymd_regex = r"_(\d{4})_(\d{2})_(\d{2})"
+    for file in os.listdir(folder_path):
+        if os.path.isfile(os.path.join(folder_path, file)):
+
+            year, month, day = [int(number) for number in re.findall(ymd_regex, file)[0]]
+            dates.append(date(year, month, day))
+
+    return dates
+
+
+def get_day_of_the_year(dates: list[date]) -> list[int]:
+    """Get the day of the year"""
+    days = [d.timetuple().tm_yday for d in dates]
+
+    return days 
+
+
+def select_datavars(dataset: xr.Dataset) -> xr.Dataset:
+    """
+    Select only relevant variables (for Brightness temperature)
+    """
+
+    selected_dvars = [
+        "longitude",
+        "latitude",
+        "node",  # node of swath
+        # "look", # look direction. we will select only look = 0 (forward)
+        "frequency_vpol",  # center frequency of V-pol channel in each band
+        "frequency_hpol",  # center frequency of H-pol channel in each band
+        "eia_nominal",  # nominal Earth indidence angle of each band
+        "time",  # Time of observation (lat, lon) seconds since 01 JAN 2000 00Z
+        "eaa",  # boresight Earth azimuth angle. range: [0o, 360o].
+        "eia",  #  boresight Earth incidence angle. range: [0o, 90o]
+        "tbtoa",  # Brightness temperature
+        "quality_flag",  # 32-bit quality control flag
+        # "sss_HYCOM", # HYCOM sea surface salinity
+        # "surtep_REY", # NOAA (Reynolds) V2 OI sea surface temperature
+        # # Land fractions
+        # "fland_06", # for 6GHz
+        # "fland_10", # For 10 GHz
+        # # Windsat V8 products
+        # "surtep_WSAT", # skin temperature
+        # "colvap_WSAT", # atmosphere_mass_content_of_water_vapor
+        # "colcld_WSAT", # atmosphere_mass_content_of_cloud_liquid_water
+        # "winspd_WSAT", # sea surface wind speed
+        # "rain_WSAT", # surface rain rate
+        # # Cross-Calibrated Multi-Platform
+        # "winspd_CCMP", # Wind speed
+        # "windir_CCMP", # Cross-Calibrated Multi-Platform Wind direction
+        # # ERA 5 products
+        # "surtep_ERA5", # skin temperature
+        # "airtep_ERA5", # Air temperature at 2m above surface
+        # "colvap_ERA5", # Columnar liquid cloud water
+        # "colcld_ERA5", # atmosphere_mass_content_of_cloud_liquid_water
+        # "winspd_ERA5", # 10-m NS wind speed
+        # "windir_ERA5", # Wind direction
+        # "surtep_CMC", # CMC Sea surface temperature
+        # "rain_IMERG", # IMERG V6 surface rain rate
+        # # RSS 2022 absorption model
+        # "tran", # Total atmospheric transmittance computed from ERA atmospheric profiles and WSAT columnar vapor and cloud water
+        # "tbdw", # Atmospheric downwelling brightness temperature computed from ERA atmospheric profiles and WSAT columnar vapor and cloud water
+    ]
+
+    return dataset[selected_dvars]
+
+
+def select_dims(dataset: xr.Dataset) -> xr.Dataset:
+    """
+    Remove unused frequencies and polarizations
+
+    Frequencies:
+    0 -- 6.8 GHz
+    1 -- 10.7 GHz
+    2 -- 18.7 GHz (Ku)
+    3 -- 23.8 GHz
+    4 -- 37.0 GHz (Ka)
+
+    Polarizations:
+    0 -- V
+    1 -- H
+    (except for 6.8 and 23.8 GHz):
+    2 -- P (+45º)
+    3 -- M (-45º)
+    4 -- L (Circular Left)
+    5 -- R (Circular Right)
+    """
+
+    # Select dimensions
+    dataset = dataset.sel(
+        indexers={
+            "polarization": [0, 1],  # [ V, H ]
+            "frequency_band": [2, 4],  # [ 18.7 GHz (Ku) , 37.0 GHz (Ka) ]
+            "look_direction": 0,  # Forward
+        }
+    )
+    return dataset
+
+
+def transform_dataset(dataset: xr.Dataset) -> xr.Dataset:
+    """
+    Other transformations
+    """
+    # Roll lattitude grid so we have -180, 180 range
+    dataset = dataset.roll(shifts={"longitude_grid": 4 * 180})
+
+    # Extract latitude and longitude grid dimensions
+    dataset = dataset.assign_coords(lat=dataset.latitude, lon=dataset.longitude)
+
+    return dataset
+
+
+def preporcess_dataset(dataset: xr.Dataset) -> xr.Dataset:
+    """Wrapper"""
+    dataset = select_datavars(dataset)
+    dataset = select_dims(dataset)
+    dataset = transform_dataset(dataset)
+    return dataset
+
+
+_preprocess_dataset = partial(preporcess_dataset)
+
+
+def windsat_datacube(folder_path: str) -> xr.Dataset:
+    """ 
+        Wrapper for creating a dataset with the combined data inside a folder
+        param folder_path: must contain the files in .nc format
+    """
+
+    dates = recover_dates(folder_path)
+    day_numbers = get_day_of_the_year(dates)
+
+    ds = xr.open_mfdataset(
+        paths= folder_path + "\\*.nc",
+        preprocess=_preprocess_dataset,
+        decode_times=False,
+        concat_dim="day_number",
+        combine="nested",
+    )
+
+    # Add a day_number coordinate
+    ds["day_number"] = day_numbers
+    ds["day_number"].attrs = {f"Description": "Int, day of the year {dates[0].year}"}
+
+    return ds
