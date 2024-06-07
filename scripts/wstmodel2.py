a363d5a1720727d678acdfe9f5dbc7a4778d9fd1
andre <andrescaudiel@gmail.com>
2024-06-07 13:19:59 +0200

    training batch up to 2048

--- scripts/wstmodel2.py
+++ scripts/wstmodel2.py
@@ -182,7 +182,7 @@ if __name__ == "__main__":
         x_train,
         y_train,
         epochs=1000,
-        batch_size = 1024,
+        batch_size = 2048,
         validation_data=(x_test,y_test),
         callbacks=[callback, checkpoints],
         verbose = 2
