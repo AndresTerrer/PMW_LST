26fac5195767a4234823bb6723d5f0074df44921
andre <andrescaudiel@gmail.com>
2024-07-19 13:00:16 +0200

    using new functions

--- scripts/wstmodel1.py
+++ scripts/wstmodel1.py
@@ -19,26 +19,30 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.processing import windsat_datacube, model_preprocess
-from src.model import transform_batch, xy_split
+from src.model import xy_split, create_training_df
 
 # OS Params
 params = ArgumentParser()
 params.add_argument(
-    "--source_folder", default="./data/raw/Daily_Windsat/", help= "Folder with Windsat dataset."
+    "--source_folder", default="./data/raw/daily_Windsat/", help= "Folder with Windsat dataset."
 )
 
 params.add_argument(
     "--output_folder", default= "./models/", help= "Folder to store final weights and trining history."
 )
 
-def build_model(info:bool = False):
-    n_vars = ascds_df.shape[1] - 1 # dont count the prediction column.
+# Swath selection
+params.add_argument(
+    "--swath_sector", default= 0 , help = "Train a model on Ascending (0, default) or Descending (1) pass data."
+)
+
+def build_model(n_vars: int , info:bool = False):
     model = Sequential([
         Input((n_vars,)),
         BatchNormalization(),
-        Dense(30,activation="linear", name = "hiddenLayer1"),
-        Dense(20,activation="relu", name = "hiddenLayer2"),
-        Dense(10,activation="relu", name = "hiddenLayer3"),
+        Dense(60,activation="linear", name = "hiddenLayer1"),
+        Dense(30,activation="relu", name = "hiddenLayer2"),
+        Dense(15,activation="relu", name = "hiddenLayer3"),
         Dense(1,activation="relu", name = "outputLayer")
     ])
     model.compile(
@@ -59,57 +63,61 @@ if __name__ == "__main__":
 
     folder_path = args.source_folder
     output_folder = args.output_folder
+    swath_sector = int(args.swath_sector)
+
+    swath2char = {
+        0 : "A", # Ascensing pass (6 PM)
+        1 : "D", # Descending pass (6 AM)
+    }
 
     print("--- Windsat datacube model training --- ")
+
+    print(f"Swath sector '{swath2char[swath_sector]}' ")
 
     #Load the dataset from the folder
     print(f"Loading windsat Datacube from {folder_path}")
     ds = windsat_datacube(folder_path)
 
     print("Processing data ...")
-    ascds = model_preprocess(ds)
+    ds = model_preprocess(
+        ds,
+        swath_sector=swath_sector,
+        look="impute",
+        add_look_flag=False
+    )
 
-    ascds_df = ascds.to_dataframe()
-    ascds_df.reset_index(inplace=True)
-    ascds_df.dropna(inplace=True)
-    ascds_df.drop(columns=["longitude_grid","latitude_grid"], inplace=True)
-    ascds_df = transform_batch(ascds_df)
+    df = create_training_df(ds)
 
-    # Remove the time of observation TEMPORARY, FOR TESTING
-    ascds_df = ascds_df[
-        [
-            col for col in ascds_df.columns if col not in 
-            ["time_18Ghz","time_37Ghz","day_number"]
-        ]
-    ]
     print(f"Training variables:")
     message = " -> "
-    for col in ascds_df.columns:
+    for col in df.columns:
         message += col + "  "
     print(message)
-    model = build_model(info=True)
+
+    n_vars = df.shape[1] - 1
+    model = build_model(n_vars, info=True)
 
     # Pick the columns for training and test
-    X, y = xy_split(ascds_df)
+    X, y = xy_split(df, y_column="surtep_ERA5")
     x_train, x_test, y_train, y_test = train_test_split(X,y, test_size = 0.3, random_state = 13)
 
     # Callbacks
     callback = EarlyStopping(
         monitor = "loss",
-        patience = 5,
+        patience = 30,
         min_delta = 0.05,
         verbose=2,
         restore_best_weights = True
     )
     checkpoints = ModelCheckpoint(
         filepath = os.path.join(output_folder, "checkpoint.keras"),
-        verbose = 1
+        verbose = 2
     )
 
     history = model.fit(
         x_train,
         y_train,
-        epochs=100,
+        epochs=300,
         batch_size = 512,
         validation_data=(x_test,y_test),
         callbacks=[callback, checkpoints],
